bc0786d13ab40820ebbc10e94f1b97ea4d026365
Emil <emil.melms94@hotmail.com>
2024-11-07 14:03:17 +0100

    Hellooo! Hellooo!! Hellooo!!! Hellooo!! Hellooo!

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -2,8 +2,19 @@ package HelloWorld;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        System.out.println("Hello World!!");
-        System.out.println("Hello World!!!");
+
+        String wow = "!";
+
+        for (int i = 1; i < 5; ++i) {
+
+            System.out.print("Hello World");
+            System.out.println(wow);
+            wow += "!";
+        }
+        for (int i = 4; i >= 1; i--) {
+
+            System.out.print("Hello World");
+            System.out.println("!".repeat(i));
+        }
     }
 }
